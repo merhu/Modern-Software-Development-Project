185810aba40f3930f3bddf8d0fbafeb1e32686dd
merhu <merryamba@gmail.com>
2020-02-13 09:03:23 -0500

    fixed event controller

--- project/src/main/java/com/bah/controller/EventController.java
+++ project/src/main/java/com/bah/controller/EventController.java
@@ -29,7 +29,7 @@ public class EventController {
 	@Autowired
 	EventRepo eventRepo;
 	
-	// get event object
+	// get event object from event controller 
 	@GetMapping
 	public Iterable<Event> getAll(){
 		return eventRepo.findAll();
